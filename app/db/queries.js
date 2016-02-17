856ecbb33bce1dca6b5703030671f42c2a9495e6
Anish Basu <anishbasu@acm.org>
2016-02-16 22:47:15 -0500

    Added Templates to Render in Routes

--- app/db/queries.js
+++ app/db/queries.js
@@ -2,6 +2,7 @@ module.exports = {
     new_experiment: "INSERT INTO experiment(name,description,subject_data_form) values(${name},${description},$(form_data))",
     list_experiments: "SELECT * FROM experiment",
     get_subjects: "SELECT id FROM subject WHERE experiment=${experiment}",
-    new_subject: "INSERT INTO subject(experiment,device_setup,subject_data) values(${id},${device_setup},${subject_data})",
-    
+    get_channel_by_subject_id: "SELECT DISTINCT channel.fk_source FROM public.channel, public.raw_data WHERE raw_data.fk_channel = channel.pk_channel_id AND raw_data.fk_subject = ${subject};",
+
+
 }
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,24 +1,22 @@
 module.exports = function(app){
     app.get('/', function(req,res) {
-        res.json({
-
-        })
+        res.render('index',{});
     });
 
     app.get('experiments/', function(req, res) {
-
+        res.render('experiments',{});
     });
 
     app.get('experiment/:id', function(req, res) {
-
+        res.render('experiment',{});
     });
 
     app.post('experiment/:id/subject/:sub_id/getData', function(req, res) {
-
+        res.render('getdata',{})
     });
 
     app.get('experiment/:id/subject/:sub_id/getStimulus', function(req, res) {
-
+        res.render('getstimulus',{})
     });
 
 }
